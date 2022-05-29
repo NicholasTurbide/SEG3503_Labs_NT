589561a652589b6bb46f5908b604a8ee6df9aac2
Nicholas Turbide <nicholas.turbide2@gmail.com>
2022-05-28 21:30:36 -0400

    adding junit

---
+++ junit5-main/junit-jupiter-engine/src/test/java/org/junit/jupiter/api/condition/EnabledOnOsConditionTests.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2015-2022 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.api.condition;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onAix;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onFreebsd;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onLinux;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onMac;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onOpenbsd;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onSolaris;
+import static org.junit.jupiter.api.condition.EnabledOnOsIntegrationTests.onWindows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.platform.commons.PreconditionViolationException;
+
+/**
+ * Unit tests for {@link EnabledOnOsCondition}.
+ *
+ * <p>Note that test method names MUST match the test method names in
+ * {@link EnabledOnOsIntegrationTests}.
+ *
+ * @since 5.1
+ */
+class EnabledOnOsConditionTests extends AbstractExecutionConditionTests {
+
+	private static final String OS_NAME = System.getProperty("os.name");
+
+	@Override
+	protected ExecutionCondition getExecutionCondition() {
+		return new EnabledOnOsCondition();
+	}
+
+	@Override
+	protected Class<?> getTestClass() {
+		return EnabledOnOsIntegrationTests.class;
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#enabledBecauseAnnotationIsNotPresent()
+	 */
+	@Test
+	void enabledBecauseAnnotationIsNotPresent() {
+		evaluateCondition();
+		assertEnabled();
+		assertReasonContains("@EnabledOnOs is not present");
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#missingOsDeclaration()
+	 */
+	@Test
+	void missingOsDeclaration() {
+		Exception exception = assertThrows(PreconditionViolationException.class, this::evaluateCondition);
+		assertThat(exception).hasMessageContaining("You must declare at least one OS");
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#enabledOnEveryOs()
+	 */
+	@Test
+	void enabledOnEveryOs() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(true);
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#aix()
+	 */
+	@Test
+	void aix() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onAix());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#freebsd()
+	 */
+	@Test
+	void freebsd() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onFreebsd());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#linux()
+	 */
+	@Test
+	void linux() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onLinux());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#macOs()
+	 */
+	@Test
+	void macOs() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onMac());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#macOsWithComposedAnnotation()
+	 */
+	@Test
+	void macOsWithComposedAnnotation() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onMac());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#openbsd()
+	 */
+	@Test
+	void openbsd() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onOpenbsd());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#windows()
+	 */
+	@Test
+	void windows() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onWindows());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#solaris()
+	 */
+	@Test
+	void solaris() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(onSolaris());
+	}
+
+	/**
+	 * @see EnabledOnOsIntegrationTests#other()
+	 */
+	@Test
+	void other() {
+		evaluateCondition();
+		assertEnabledOnCurrentOsIf(
+			!(onAix() || onFreebsd() || onLinux() || onMac() || onOpenbsd() || onSolaris() || onWindows()));
+		assertCustomDisabledReasonIs("Disabled on almost every OS");
+	}
+
+	private void assertEnabledOnCurrentOsIf(boolean condition) {
+		if (condition) {
+			assertEnabled();
+			assertReasonContains("Enabled on operating system: " + OS_NAME);
+		}
+		else {
+			assertDisabled();
+			assertReasonContains("Disabled on operating system: " + OS_NAME);
+		}
+	}
+
+}
---
+++ junit5-main/junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/MethodArgumentsProvider.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2015-2022 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.params.provider;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.support.AnnotationConsumer;
+import org.junit.platform.commons.JUnitException;
+import org.junit.platform.commons.util.CollectionUtils;
+import org.junit.platform.commons.util.Preconditions;
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.junit.platform.commons.util.StringUtils;
+
+/**
+ * @since 5.0
+ */
+class MethodArgumentsProvider implements ArgumentsProvider, AnnotationConsumer<MethodSource> {
+
+	private String[] methodNames;
+
+	@Override
+	public void accept(MethodSource annotation) {
+		this.methodNames = annotation.value();
+	}
+
+	@Override
+	public Stream<Arguments> provideArguments(ExtensionContext context) {
+		Object testInstance = context.getTestInstance().orElse(null);
+		// @formatter:off
+		return Arrays.stream(this.methodNames)
+				.map(factoryMethodName -> getMethod(context, factoryMethodName))
+				.map(method -> context.getExecutableInvoker().invoke(method, testInstance))
+				.flatMap(CollectionUtils::toStream)
+				.map(MethodArgumentsProvider::toArguments);
+		// @formatter:on
+	}
+
+	private Method getMethod(ExtensionContext context, String factoryMethodName) {
+		if (StringUtils.isBlank(factoryMethodName)) {
+			return ReflectionUtils.getRequiredMethod(context.getRequiredTestClass(),
+				context.getRequiredTestMethod().getName());
+		}
+		if (factoryMethodName.contains(".") || factoryMethodName.contains("#")) {
+			return getMethodByFullyQualifiedName(factoryMethodName);
+		}
+		return getMethodByShortName(context.getRequiredTestClass(), factoryMethodName);
+	}
+
+	private Method getMethodByFullyQualifiedName(String fullyQualifiedMethodName) {
+		String[] methodParts = ReflectionUtils.parseFullyQualifiedMethodName(fullyQualifiedMethodName);
+		String className = methodParts[0];
+		String methodName = methodParts[1];
+		String methodParameters = methodParts[2];
+
+		return ReflectionUtils.findMethod(loadRequiredClass(className), methodName, methodParameters).orElseThrow(
+			() -> new JUnitException(
+				format("Could not find method [%s(%s)] in class [%s]", methodName, methodParameters, className)));
+	}
+
+	private Method getMethodByShortName(Class<?> testClass, String methodName) {
+		List<Method> methods = ReflectionUtils.findMethods(testClass, method -> method.getName().equals(methodName));
+		Preconditions.condition(methods.size() > 0,
+			() -> format("Could not find method [%s] in class [%s]", methodName, testClass.getName()));
+		Preconditions.condition(methods.size() <= 1,
+			() -> format("Several factory methods named [%s] were found in class [%s]", methodName,
+				testClass.getName()));
+		return methods.get(0);
+	}
+
+	private Class<?> loadRequiredClass(String className) {
+		return ReflectionUtils.tryToLoadClass(className).getOrThrow(
+			cause -> new JUnitException(format("Could not load class [%s]", className), cause));
+	}
+
+	private static Arguments toArguments(Object item) {
+
+		// Nothing to do except cast.
+		if (item instanceof Arguments) {
+			return (Arguments) item;
+		}
+
+		// Pass all multidimensional arrays "as is", in contrast to Object[].
+		// See https://github.com/junit-team/junit5/issues/1665
+		if (ReflectionUtils.isMultidimensionalArray(item)) {
+			return arguments(item);
+		}
+
+		// Special treatment for one-dimensional reference arrays.
+		// See https://github.com/junit-team/junit5/issues/1665
+		if (item instanceof Object[]) {
+			return arguments((Object[]) item);
+		}
+
+		// Pass everything else "as is".
+		return arguments(item);
+	}
+
+}
