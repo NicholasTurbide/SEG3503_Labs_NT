49692998863323ac11d8fc18c815960452223f50
Nicholas Turbide <nicholas.turbide2@gmail.com>
2022-06-22 21:51:59 -0400

    adding first test hopefully

---
+++ Lab4/fizzbuzz_java/src/fizzbuzz.java
@@ -0,0 +1,20 @@
+public class Fizzbuzz {
+    
+    public String fizz(int n){
+        str modulo;
+
+        if(n % 3 == 0){
+            modulo = "fizz";
+        }
+        
+        if(n % 5 == 0){
+            modulo = "buzz";
+        }
+
+        if(n % 3 != 0 && n % 5 != 0){
+           modulo = Integer.toString(n);
+        }
+
+        return modulo;
+    }
+}
