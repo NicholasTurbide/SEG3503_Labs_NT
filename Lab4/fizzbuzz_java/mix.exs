dabc6f33aea5e407cb026b7d8f281fce70b470bf
Nicholas Turbide <nicholas.turbide2@gmail.com>
2022-06-22 21:44:01 -0400

    updates

--- Lab4/fizzbuzz_java/mix.exs
+++ Lab4/fizzbuzz_java/mix.exs
@@ -1,9 +1,9 @@
-defmodule Tic.MixProject do
+defmodule Fizzbuzz.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :tic,
+      app: :fizzbuzz,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
